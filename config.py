6f827a91ff53ea5a4394047441da08fb1c5959b4
dragokepapa <eriyerox683@gmail.com>
2026-04-02 13:54:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py mein yeh sab fill karo
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+
+# MongoDB (neeche bataya hai kaise setup karna)
+MONGO_URI = "mongodb+srv://nexacoders2_db_user:dxYh7QOdHvH6OVdd@cluster0.f4qxcbk.mongodb.net/?appName=Cluster0"
+
+# Optional settings
+ADMIN_IDS = [ 8669209490 ]          # Apna user ID daal sakte ho
+DEBUG = False
